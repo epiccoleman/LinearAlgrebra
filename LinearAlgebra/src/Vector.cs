ebadfaa71e63390d5a1df63febee2c3eae99cf3b
Eric Coleman <agentwiggles@gmail.com>
2016-09-10 17:03:12 -0400

    Refactored to 3d

--- LinearAlgebra/src/Vector.cs
+++ LinearAlgebra/src/Vector.cs
@@ -11,43 +11,40 @@ namespace LinearAlgebra
 
         private double m_x;
         private double m_y;
+        private double m_z;
 
         public double x
         {
-            get
-            {
-               return m_x;
-            }
-            set
-            {
-               m_x = value;
-            }
+            get { return m_x; }
+            set { m_x = value; }
         }
 
         public double y
         {
-            get
-            {
-                return m_y;
-            }
-            set
-            {
-                m_y = value;
-            }
-        } 
+            get { return m_y; }
+            set { m_y = value; }
+        }
 
-        public Vector(double x = 0d, double y = 0d)
+        public double z
+        {
+            get { return m_z; }
+            set { m_z = value; }
+        }
+
+        public Vector(double x = 0d, double y = 0d, double z = 0d)
         {
             m_x = x;
             m_y = y;
+            m_z = z;
         }
 
         public static Vector operator +(Vector v1, Vector v2)
         {
             double new_x = v1.x + v2.x;
             double new_y = v1.y + v2.y;
+            double new_z = v1.z + v2.z;
 
-            return new Vector(new_x, new_y);
+            return new Vector(new_x, new_y, new_z);
         }
 
         public static bool operator ==(Vector v1, Vector v2)
@@ -73,7 +70,7 @@ namespace LinearAlgebra
                 return false;
             }
 
-            return x == other.x && y == other.y;
+            return x == other.x && y == other.y && z == other.z;
         }
 
         public override int GetHashCode()
