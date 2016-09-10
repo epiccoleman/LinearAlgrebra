67b1fc7807e69f5abac085d2338bd00008d53706
Eric Coleman <agentwiggles@gmail.com>
2016-09-10 16:46:32 -0400

    Implement addition and equality

--- LinearAlgebra/src/Vector.cs
+++ LinearAlgebra/src/Vector.cs
@@ -41,5 +41,44 @@ namespace LinearAlgebra
             m_x = x;
             m_y = y;
         }
+
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            double new_x = v1.x + v2.x;
+            double new_y = v1.y + v2.y;
+
+            return new Vector(new_x, new_y);
+        }
+
+        public static bool operator ==(Vector v1, Vector v2)
+        {
+            return v1.Equals(v2);
+        }
+
+        public static bool operator !=(Vector v1, Vector v2)
+        {
+            return !v1.Equals(v2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            Vector other = obj as Vector;
+            if ((System.Object)other == null)
+            {
+                return false;
+            }
+
+            return x == other.x && y == other.y;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
--- LinearAlgebra/test/VectorTest.cs
+++ LinearAlgebra/test/VectorTest.cs
@@ -23,5 +23,38 @@ namespace LinearAlgebra
             Assert.AreEqual(1, vec.x);
             Assert.AreEqual(2, vec.y);
         }
+
+        [TestMethod]
+        public void VectorsCanBeAdded()
+        {
+            Vector vec1 = new Vector(1, 2);
+            Vector vec2 = new Vector(3, 4);
+
+            Vector expectedVec = new Vector(4, 6);
+            Vector actualVec = vec1 + vec2;
+
+            Assert.AreEqual(expectedVec.x, actualVec.x);
+            Assert.AreEqual(expectedVec.y, actualVec.y);
+        }
+
+        [TestMethod]
+        public void VectorsCanBeComparedForEquality()
+        {
+            Vector vec1 = new Vector(1, 1);
+            Vector vec2 = new Vector(1, 1);
+
+            Assert.IsTrue(vec1 == vec2);
+            Assert.AreEqual(vec1, vec2);
+        }
+
+        [TestMethod]
+        public void VectorsCanBeComparedForInequality()
+        {
+            Vector vec1 = new Vector(1, 1);
+            Vector vec2 = new Vector(1, 2);
+
+            Assert.IsTrue(vec1 != vec2);
+            Assert.AreNotEqual(vec1, vec2);
+        }
     }
 }
