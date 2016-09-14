28c0f891fe8ab9c41181f79934185fad660b18c9
Eric Coleman <agentwiggles@gmail.com>
2016-09-14 00:05:36 -0400

    Added a console app to the project which will eventually become a calculator of sorts

---
+++ LinearAlgebraCalc/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LinearAlgebra;
+
+namespace LinearAlgebraCalc
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            System.Console.WriteLine("hello");
+            
+
+            Vector vec1 = new Vector(8.218, -9.341);
+            Vector vec2 = new Vector(-1.129, 2.111);
+
+            System.Console.WriteLine(vec1 + vec2);
+            Console.ReadLine();
+            return 0;
+        }
+    }
+}
